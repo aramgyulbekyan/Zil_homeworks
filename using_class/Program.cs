0d1da995f09ba04546f5d5b1e974c78d246a1d94
Aramayis Gyulbekyan <aramayis.gyulbekyan@coafkids.org>
2020-09-14 14:25:35 +0400

    added class homework

---
+++ using_class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace using_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            string input = Console.ReadLine();
+            StringToIntArray array = new StringToIntArray(input);
+            int []result = array.ToIntArray(input);
+           
+            int length = result.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            int maxvalue = array.max(array.ToIntArray(input));
+            Console.WriteLine(maxvalue);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ using_class/StringToIntArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace using_class
+{
+    class StringToIntArray
+    {
+        private string field;
+        public StringToIntArray (string field)
+        {
+            this.field = field;
+        }
+
+        public int[] ToIntArray(string field)
+        {
+            int[] result = new int[field.Length];
+            for (int i = 0; i < field.Length; i++)
+            {
+                result[i] = int.Parse(field[i].ToString());
+            }
+            return result;
+        }
+        public int max (int[] input )
+        {
+            int max = (ToIntArray(field).Max(element => Math.Abs(element)));
+            return max;
+        }
+
+
+    }
+}
